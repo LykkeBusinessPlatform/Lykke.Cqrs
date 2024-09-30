61ffcd7b00d7486da9a00683fe01137bcaf7a5b7
atarutin <andrey.tarutin@lykke-business.ch>
2024-09-30 17:35:22 +0300

    test(LT-5584): disable cqrs engine tests paralellization.

--- tests/Lykke.Cqrs.Tests/CqrsEngineTests.cs
+++ tests/Lykke.Cqrs.Tests/CqrsEngineTests.cs
@@ -17,6 +17,7 @@ using System.Threading.Tasks;
 namespace Lykke.Cqrs.Tests
 {
     [TestFixture]
+    [NonParallelizable]
     internal class CqrsEngineTests
     {
         private readonly ILogFactory _logFactory;
